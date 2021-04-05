8e90464333ee5c7cf0402130a303dfaf4c55e919
luke0619s <950203@nkps.tp.edu.tw>
2021-04-06 02:38:24 +0800

    3 push

--- Mosquito/Mosquito/wsgi.py
+++ Mosquito/Mosquito/wsgi.py
@@ -1,19 +1,9 @@
-"""
-WSGI config for Mosquito project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-
-application = get_wsgi_application()
+from dj_static import Cling
 from Mosquito.settings import DEBUG
+application = get_wsgi_application()
 if not DEBUG:
     from dj_static import Cling
     application = Cling(get_wsgi_application())
