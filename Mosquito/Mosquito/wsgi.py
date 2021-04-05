c74caf8c00e246e10462f0b1ac42e18a5e6a2613
luke0619s <950203@nkps.tp.edu.tw>
2021-04-06 03:49:22 +0800

    8push

--- Mosquito/Mosquito/wsgi.py
+++ Mosquito/Mosquito/wsgi.py
@@ -1,13 +1,12 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-from Mosquito.settings import DEBUG
-application = get_wsgi_application()
-if not DEBUG:
-    from dj_static import Cling
-    application = Cling(get_wsgi_application())
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mosquito.settings')
 
 application = get_wsgi_application()
+
+from Mosquito.settings import DEBUG
+if not DEBUG:
+    from dj_static import Cling
+    application = Cling(get_wsgi_application())
